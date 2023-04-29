35eee45fb65eb01717397e319dc1629cb45e8768
David Garcia <carlosdavidgarcialopez@gmail.com>
2023-04-28 21:42:34 -0500

    change index page

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Box, Card, CardContent, Container, Grid, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Index = () => {
     return (
-        <div>Index</div>
+        <Container>
+            <Typography variant='h2' textAlign={'center'}>MainSoft - Humedad por ciudades</Typography>
+            <Typography textAlign={'center'}>
+                <Link to={'/search'}>Inicia tu busqueda aqui.</Link>
+            </Typography>
+        </Container>
     )
 }
 
